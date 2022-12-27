baebcd4fa1c6302f4c43b2fb7239f8693fadc4e5
Shah <shahmansurov1001@gmail.com>
2022-12-27 14:22:20 -0500

    working on categorySlice

--- src/core/redux/auth/authResponses.ts
+++ src/core/redux/auth/authResponses.ts
@@ -12,19 +12,3 @@ export interface fetchResponse {
   email: string;
   avatar: string;
 }
-
-export interface DataResponse {
-  first_name: string;
-  last_name: string;
-  email: string;
-  avatar: string;
-  google_id: string;
-}
-
-export interface DataResponse {
-  first_name: string;
-  last_name: string;
-  email: string;
-  avatar: string;
-  google_id: string;
-}
